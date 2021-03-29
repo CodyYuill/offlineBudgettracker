8fd996d88a0a872b223250cf390ac2a2ad8f97f2
Cody Yuill <codyyuill@hotmail.com>
2021-03-29 16:04:00 -0400

    added indexedDB.js to fall back to indexed on no connection

---
+++ public/indexedDB.js
@@ -0,0 +1,59 @@
+var db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  const pendingItems = store.getAll();
+
+  pendingItems.onsuccess = function () {
+    if (pendingItems.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(pendingItems.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        }
+      }).then((response) => {
+          return response.json();
+        }).then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+
+          const store = transaction.objectStore("pending");
+
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
